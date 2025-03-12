587f4911d406fa528d96fd7a461eba6462a86b7a
Mohamed Mahmodi <103428364+MohammedMMC@users.noreply.github.com>
2025-03-12 23:01:44 +0300

    changed files style, and removed themes folder

--- scripts/levels.gd
+++ scripts/levels.gd
@@ -4,4 +4,4 @@ func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_level_button_pressed(level_number: int) -> void:
-	get_tree().change_scene_to_file("res://scenes/game%d.tscn" % level_number)
+	get_tree().change_scene_to_file("res://scenes/levels/level%d.tscn" % level_number)
