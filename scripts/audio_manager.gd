fb02e44e007bea385cc4ec446faf1e47d62b7314
Mohamed Mahmodi <103428364+MohammedMMC@users.noreply.github.com>
2025-03-30 22:13:43 +0300

    fixed music loop

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -5,17 +5,18 @@ var sfx_player: AudioStreamPlayer
 
 func _ready():
 	music_player = AudioStreamPlayer.new()
-	var music_stream = load("res://audio/bg.wav") 
+	var music_stream = load("res://audio/bg.wav")
 	
-	music_stream.loop = true
 	music_player.stream = music_stream
 	music_player.volume_db = -2
-	music_player.bus = "Music" 
+	music_player.bus = "Music"
 	music_player.autoplay = true
-	music_player.set_stream_paused(false)  
+	music_player.set_stream_paused(false)
+	music_player.finished.connect(music_player.play)
 	
 	add_child(music_player)
 	music_player.play()
+	
 	sfx_player = AudioStreamPlayer.new()
 	sfx_player.bus = "SFX"
 	add_child(sfx_player)
