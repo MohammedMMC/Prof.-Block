1b9c366348f37a5bec12acdc857cb5bf2ebda412
Mohamed Mahmodi <103428364+MohammedMMC@users.noreply.github.com>
2025-04-05 11:19:52 +0300

    helper popup animation

--- scripts/helpers.gd
+++ scripts/helpers.gd
@@ -5,6 +5,8 @@ extends Control
 
 @onready var helpers = [$Help1, $Help2, $Help3, $Help4]
 
+@onready var animations_player = $AnimationPlayer
+
 func _ready() -> void:
 	for helper in helpers:
 		helper.hide()
@@ -12,8 +14,11 @@ func _ready() -> void:
 	if help_id > 0 and help_id <= helpers.size():
 		self.show()
 		helpers[help_id - 1].show()
-	
+		
+	animations_player.play("popup")
 
 func _on_back_button_pressed() -> void:
+	animations_player.play_backwards("popup")
+	await animations_player.animation_finished
 	self.hide()
 	self.queue_free()
